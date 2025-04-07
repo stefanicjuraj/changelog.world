9b98ef0b38bcd224f366a13688bd620845e5aa7c
Juraj Stefanic <jurajstefanic@outlook.com>
2025-04-07 14:02:54 +0200

    feat: add env type

---
+++ types/env.d.ts
@@ -0,0 +1,23 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    FEED_URL_REACT: string;
+    FEED_URL_NEXTJS: string;
+    FEED_URL_TAILWIND: string;
+    FEED_URL_VERCEL: string;
+    FEED_URL_SVELTE: string;
+    FEED_URL_VUEJS: string;
+    FEED_URL_GO: string;
+    FEED_URL_PYTHON: string;
+    FEED_URL_PHP: string;
+    FEED_URL_SWIFT: string;
+    FEED_URL_RAILS: string;
+    FEED_URL_ANGULAR: string;
+    FEED_URL_LARAVEL: string;
+    FEED_URL_DJANGO: string;
+    FEED_URL_CPP: string;
+    FEED_URL_GITHUB: string;
+    FEED_URL_JAVA: string;
+    FEED_URL_EXPRESS: string;
+    FEED_URL_SPRING_BOOT: string;
+  }
+}
